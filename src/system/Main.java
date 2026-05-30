ae402793ccf1be713498e0018589b2553af0535e
Anísio Oliveira <anisioalbuquerque71@gmail.com>
2026-05-30 12:14:46 -0300

    Main atualizada

--- src/system/Main.java
+++ src/system/Main.java
@@ -3,6 +3,7 @@ package system;
 public class Main {
 
 	public static void main(String[] args) {
+		 System.out.println("=== Sistema sem Adapter (passo 1) ===\n");
 		SistemaNotificacao sistema = new SistemaNotificacao();
 		sistema.notificar("joao@email.com", "Seu cadastro foi confirmado.");
 		sistema.notificar("admin@empresa.com", "Falha de autenticação detectada.");
